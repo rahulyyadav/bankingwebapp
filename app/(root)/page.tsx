67e0273a79abc5e6ceda74ae0bd1f08a7c777eb2
Rahul Yadav <rahulyyadav21@gmail.com>
2024-09-13 11:25:21 +0545

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { LassoSelect } from 'lucide-react'
+
 
 const Home = () => {
+  const loggedIn = {firstName: 'Rahul', lastName: 'Yadav', email: 'rahulyyadav21@gmail.com'};
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and Manage your account and transactions efficiently."
+          />
+
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={4340.35}
+
+        />
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 1250.30 },{currentBalance: 2345.56 }]}
+      />
+    </section>
   )
 }
 

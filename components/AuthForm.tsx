6dba792d7fe882e5e05859bcc2295a4bc4d3717a
Rahul Yadav <rahulyyadav21@gmail.com>
2024-09-15 16:56:15 +0545

    Implement Plaid and dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,6 +22,7 @@ import CustomInput from "./CustomInput"
 import { authFormSchema } from "@/lib/utils"
 import { useRouter } from "next/navigation"
 import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions"
+import PlaidLink from "./PlaidLink"
 
 
   
@@ -51,8 +52,22 @@ const AuthForm = ({type}: {type: string}) => {
         try {
             // Sign up with Appwrite & create plaid token
 
+            
             if(type === 'sign-up'){
-               const newUser = await signUp(data);
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password,
+
+                }
+               const newUser = await signUp(userData);
 
                setUser(newUser);
             }
@@ -108,7 +123,7 @@ const AuthForm = ({type}: {type: string}) => {
         </header>
         {user ? (
             <div className='flex flex-col gap-4'>
-                {/* PlaidLink */}
+                <PlaidLink user={user} variant="primary"  />
             </div>
         ): (
             <>
@@ -145,7 +160,7 @@ const AuthForm = ({type}: {type: string}) => {
                             </div>
                             <div className="flex gap-4">
                                 <CustomInput 
-                                    control={form.control} name='dob' label='Date of Birth' placeholder='yyyy-mm-dd'
+                                    control={form.control} name='dateOfBirth' label='Date of Birth' placeholder='yyyy-mm-dd'
                                 />
                                 <CustomInput 
                                     control={form.control} name='ssn' label='SSN' placeholder='ex: 1234'
